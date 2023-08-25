2b138fcf4237690f67c441b5d4204db32af31646
Antr <avisiannari@gmail.com>
2023-08-25 08:48:11 +0300

    Repair v1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,31 @@
 import java.lang.Error
 
-const val Type_Card = "" // Варианты карт: MC = Master Card, Mir, Visa, Maestro
+const val Type_Card = "MC" // Варианты карт: MC = Master Card, Mir, Visa, Maestro
 const val Error_Wrong_Type = -1.0
 const val Error_Limit_Exceeded = -2.0
-const val transfer = 10000
-const val previos = 12345
+const val transfer = 2000
+const val previos = 0
 var comission = transfer / 100 * 0.75
+var difference = (previos + transfer) - 75_000
 fun main() {
+    println(comissionAll(transfer))
 
+}
+fun comissionAll(transfer: Int, previous:Int = 0, typeCard: String = "VK"): Double {
     when {
-        Type_Card === "MC" -> println(comissionMC(Type_Card, transfer, previos))
-        Type_Card === "Maestro" -> println(comissionMC(Type_Card, transfer, previos))
-        Type_Card === "Mir" -> println(comissionVisa(Type_Card, transfer, previos))
-        Type_Card === "Visa" -> println(comissionVisa(Type_Card, transfer, previos))
-        else -> println(comissionVK(Type_Card, transfer, previos))
+        Type_Card === "MC" -> return comissionMC(Type_Card, transfer, previos)
+        Type_Card === "Maestro" -> return comissionMC(Type_Card, transfer, previos)
+        Type_Card === "Mir" -> return comissionVisa(Type_Card, transfer, previos)
+        Type_Card === "Visa" -> return comissionVisa(Type_Card, transfer, previos)
+        else -> return comissionVK(Type_Card, transfer, previos)
+    }
+
+}
+fun comissionOnMC (transfer: Int,previos: Int):Double {
+    return when {
+        previos > 75_000 -> (transfer * 0.006) + 20
+        transfer + previos > 75_000 -> (difference * 0.006) + 20
+        else -> 0.0
     }
 }
 
@@ -21,8 +33,8 @@ fun comissionMC(typeCard: String, transfer: Int, previos: Int): Double {
     return when (typeCard) {
         Type_Card -> if (transfer > 150_000 || transfer + previos > 600_000) {
             Error_Limit_Exceeded
-        } else {
-            if (transfer <= 75_000) 0.0 else (transfer * 0.006) + 20
+        }else{
+            comissionOnMC(transfer,previos)
         }
 
         else -> Error_Wrong_Type
